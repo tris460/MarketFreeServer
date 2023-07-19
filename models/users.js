c513b772658f2fd21a2bfe7b63d5ab8c519cd9ea
Beatriz Martinez Perez <trism460@gmail.com>
2023-07-18 20:37:08 -0600

    Add address to user

--- models/users.js
+++ models/users.js
@@ -15,6 +15,10 @@ const userSchema = new Schema({
     type: String,
     required: [true]
   },
+  address: {
+    type: String,
+    required: [false]
+  },
   password: {
     type: String,
     required: [true]
