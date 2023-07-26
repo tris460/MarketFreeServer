1a6aa8c9b6a6479a9608464b515b8c6988d38f4d
AnaLucyGDL <analucygdl@gmail.com>
2023-07-26 01:12:22 -0600

    Commented accessToken

--- routes/login.js
+++ routes/login.js
@@ -1,32 +1,30 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const router = express.Router();
 const app = express();
 const User = require("../models/users");
 
 // Ruta para el inicio de sesión
-router.post("/login", async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-
   try {
     // Buscar al usuario en la base de datos por su correo electrónico
-    const user = await User.findOne({ email });
-
+    const users = await User.findOne({ email:email });
+    console.log(users)
     // Si el usuario no existe, o la contraseña es incorrecta, retornar un mensaje de error
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    if (!users || password !=users.password) {
       return res.status(401).json({ message: "Credenciales inválidas" });
     }
 
     // Si las credenciales son válidas, generar el token de acceso
     const accessToken = jwt.sign(
-      { userId: user._id, email: user.email },
+      { userId: users._id, email: users.email },
       "clave-secreta-del-token", // Reemplaza esto con una clave secreta segura
       { expiresIn: "1h" } // El token expirará en 1 hora
     );
 
     // Retornar el token de acceso al cliente
-    res.json({ accessToken });
+    res.status(200).json({ data: users });
   } catch (error) {
     // Si ocurre un error en el servidor, retornar un mensaje de error
     console.error("Error al intentar iniciar sesión:", error);
